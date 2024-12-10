2a952d50ba6d1d7c9ee9cbbbedea5e76a01c6a0f
Cabbache <cabbache@cabbache.net>
2024-12-10 22:25:37 +0100

    8

---
+++ 8/part_1/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs;
+use std::collections::HashSet;
+
+type Mm = Vec<Vec<char>>;
+
+fn main() {
+	let args = std::env::args().collect::<Vec<_>>();
+  let file_path = std::path::Path::new(&args[1]);
+	let input = fs::read_to_string(file_path).expect("Unable to read file");
+	let input = input.trim();
+	
+	let map: Mm = input
+		.lines()
+		.map(|l| l.chars().collect::<Vec<char>>())
+		.collect();
+
+	let mut mapclone = map.clone();
+	
+	let bands: HashSet<char> = HashSet::from_iter(input.chars().filter(|&c| c != '.'));
+	for band in bands {
+		let mut posl: Vec<(i32, i32)> = Vec::new();
+		for i in 0..map.len() {
+			for j in 0..map[0].len() {
+				if map[i][j] == band {
+					posl.push((i as i32,j as i32));
+				}
+			}
+		}
+		for (a,b) in posl.iter().flat_map(|p| posl.iter().filter(move |&q| p != q).map(move |q| (p,q))) {
+			let diff = (a.0 - b.0, a.1 - b.1);
+			let ans = vec![
+				(a.0 + diff.0, a.1 + diff.1),
+				(a.0 - diff.0, a.1 - diff.1),
+				(b.0 + diff.0, b.1 + diff.1),
+				(b.0 - diff.0, b.1 - diff.1),
+			];
+			let within = |p: &&(i32, i32)| p.0 >= 0 && p.1 >= 0 && p.0 < map.len() as i32 && p.1 < map[0].len() as i32;
+			for an in ans.iter().filter(within).filter(|&p| p != a && p != b).copied().collect::<Vec<(i32,i32)>>() {
+				mapclone[an.0 as usize][an.1 as usize] = '#';
+			}
+		}
+	}
+	for row in &mapclone {
+		println!("{}", row.iter().collect::<String>());
+	}
+	let c: usize = mapclone.iter().map(|row| row.iter().filter(|&c| *c == '#').count()).sum();
+	println!("{}",c);
+}
